f7d0457864abddab9e6acc90be076f07cc8d0cef
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2019-05-10 17:14:51 +0200

    initial commit for interface implementation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='acs-state-estimation-interfaces',
+      version='0.1',
+      description='State Estimation Interfaces',
+      author='Jan Dinkelbach',
+      author_email='acs-software@eonerc.rwth-aachen.de',
+      packages = [ 'acs.state_estimation_interfaces' ],
+      install_requires = [
+      ]
+)
