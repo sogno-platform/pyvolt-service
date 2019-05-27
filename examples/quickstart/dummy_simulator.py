86b63154aa511cbd32148f6aa85a921100eba908
martin.moraga <martin.moraga.91@gmail.com>
2019-05-27 20:50:17 +0200

    updated examples

--- examples/quickstart/dummy_simulator.py
+++ examples/quickstart/dummy_simulator.py
@@ -1,51 +1,58 @@
 import paho.mqtt.client as mqtt
 import time
 
-
-# The callback for when the client receives a CONNACK response from the server.
+def connect(client_name, username, password, broker_adress, port=1883):
+	mqttc = mqtt.Client(client_name, True)		   	
+	mqttc.username_pw_set(username, password)
+	mqttc.on_connect = on_connect					#attach function to callback
+	#mqttc.on_message = on_message					#attach function to callback
+	mqttc.connect(broker_adress, port)				#connect to broker
+	mqttc.loop_start()								#start loop to process callback
+	time.sleep(4)									#wait for connection setup to complete
+	
+	return mqttc
+	
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        client.connected_flag = True  # set flag
-        print("connected OK with returned code=", rc)
-    else:
-        print("Bad connection with returned code=", rc)
-
+	"""
+	The callback for when the client receives a CONNACK response from the server.
+	"""
+	if rc == 0:
+		print("connected OK with returned code=", rc)
+	else:
+		print("Bad connection with returned code=", rc)
 
 # parameters
 sequence = 1
 len_data = 300
+client_name = "DpsimDummy"
 topic_publish = "dpsim-powerflow"
 
-'''
 # Public Message Broker
-broker_adress = "m16.cloudmqtt.com"
-mqttc = mqtt.Client("SognoDemo_Client", True)		   	#create new instance
-mqttc.username_pw_set("ilgtdaqk", "UbNQQjmcUdqq")
-mqttc.on_connect = on_connect                   		#attach function to callback
-mqttc.connect(broker_adress, 14543)					 	#connect to broker
-'''
+broker_address = "m16.cloudmqtt.com"
+mqtt_username = "ilgtdaqk"
+mqtt_password = "UbNQQjmcUdqq"
+port = 14543
 
+"""
 # ACS Message Broker
 broker_address = "137.226.248.91"
-mqttc = mqtt.Client("DpsimDummy", True)  # create new instance
-mqttc.username_pw_set("villas", "s3c0sim4!")
-mqttc.on_connect = on_connect  # attach function to callback
-mqttc.connect(broker_address)  # connect to broker
+mqtt_username = "villas"
+mqtt_password = "s3c0sim4!"
+"""
 
-mqttc.loop_start()  # start loop to process callback
-time.sleep(1)  # wait for connection setup to complete
+mqttc = connect(client_name, mqtt_username, mqtt_password, broker_address, port)
 
 data_file = r".\sample_data\dpsim_powerflow_record_cigre.txt"
 data = []
 with open(data_file) as json_file:
-    for line in json_file:
-        data.append(line)
+	for line in json_file:
+		data.append(line)
 
 while sequence < len_data + 1:
-    mqttc.publish(topic_publish, data[sequence])
-    print("Sent data for sequence " + str(sequence) + ": " + data[sequence])
-    sequence += 1
-    time.sleep(1)
+	mqttc.publish(topic_publish, data[sequence])
+	print("Sent data for sequence " + str(sequence) + ": " + data[sequence])
+	sequence += 1
+	time.sleep(1)
 
-mqttc.loop_stop()  # Stop loop
+mqttc.loop_stop()   # Stop loop
 mqttc.disconnect()  # disconnect
--- examples/quickstart/run_state_estimation_service.py
+++ examples/quickstart/run_state_estimation_service.py
@@ -10,150 +10,215 @@ from acs.state_estimation.nv_state_estimator import DsseCall
 from acs.state_estimation.measurement import Measurents_set
 from acs.state_estimation.results import Results
 
-
 logging.basicConfig(filename='recv_client.log', level=logging.INFO, filemode='w')
 
-
-# The callback for when the client receives a CONNACK response from the server.
+def connect(client_name, username, password, broker_adress, port=1883):
+	mqttc = mqtt.Client(client_name, True)		   	
+	mqttc.username_pw_set(username, password)
+	mqttc.on_connect = on_connect					#attach function to callback
+	mqttc.on_message = on_message					#attach function to callback
+	mqttc.connect(broker_adress, port)				#connect to broker
+	mqttc.loop_start()								#start loop to process callback
+	time.sleep(4)									#wait for connection setup to complete
+	
+	return mqttc
+	
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        client.connected_flag = True  # set flag
-        print("connected OK with returned code=", rc)
-    else:
-        print("Bad connection with returned code=", rc)
+	"""
+	The callback for when the client receives a CONNACK response from the server.
+	"""
+	if rc == 0:
+		client.subscribe(topic_subscribe)
+		print("connected OK with returned code=", rc)
+	else:
+		print("Bad connection with returned code=", rc)
 
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    message = json.loads(msg.payload)[0]
-    sequence = message['sequence']
-    data = message['data']
+	"""
+	The callback for when a PUBLISH message is received from the server
+	"""
 
-    if sequence > 0:
-        # store the received data in powerflow_results
-        for node in powerflow_results.nodes:
-            magnitude = 0.0
-            phase = 0.0
-            uuid = node.topology_node.uuid
-            for elem_idx, elem_data in enumerate(mapping):
-                if elem_data[0] == uuid:  # elem_data[0] == uuid
-                    if elem_data[1] == "mag":  # elem_data[1] = "mag" or "phase"
-                        magnitude = data[elem_idx]
-                    elif elem_data[1] == "phase":
-                        phase = data[elem_idx]
-            node.voltage = magnitude * (np.cos(phase) + 1j * np.sin(phase)) / 1000
-            node.voltage_pu = node.voltage / node.topology_node.baseVoltage
+	message = json.loads(msg.payload)[0]
+	sequence = message['sequence']
+	
+	if sequence > 0:
+		#store the received data in powerflow_results
+		powerflow_results = receiveVillasNodeInput(system, message, input_mapping_vector)
+		
+		#read measurements from file
+		measurements_set = Measurents_set()
+		try:
+			if sequence < 90:
+				measurements_set.read_measurements_from_file(powerflow_results, meas_configfile1)
+				scenario_flag=1
+			else:
+				measurements_set.read_measurements_from_file(powerflow_results, meas_configfile2)
+				scenario_flag=2
 
-        # calculate quantities I, Iinj, S and Sinj
-        powerflow_results.calculate_all()
-
-        # read measurements from file
-        measurements_set = Measurents_set()
-        try:
-            if sequence < 90:
-                measurements_set.read_measurements_from_file(powerflow_results, meas_configfile1)
-            else:
-                measurements_set.read_measurements_from_file(powerflow_results, meas_configfile2)
-        except Exception as e:
-            print(e)
-        # print(measurements_set.getMeasValues())
-
-        try:
-            # calculate the measured values (affected by uncertainty)
-            measurements_set.meas_creation(dist="uniform", seed=sequence)
-            # Performs state estimation
-            state_estimation_res = DsseCall(system, measurements_set)
-        except Exception as e:
-            print(e)
-
-        # send results to message broker
-        # TODO: use former implementation as message format must be suitable for VILLASnode
-        """
-        payload = {}
-        payload["client"] = "Sogno_cigre_se_cim"
-        payload["sequence"] = sequence
-        payload["V_est_mag"] = np.absolute(state_estimation_res.get_voltages()).tolist()
-        payload["V_est_phase"] = np.angle(state_estimation_res.get_voltages()).tolist()
-        mqttc.publish(topic_publish, "[" + json.dumps(payload) + "]", 0)
-
-        Vmag_err = np.absolute(np.subtract(Vmag_est, Vmag_true))
-        Vmag_err = 100 * np.divide(Vmag_err, Vmag_true)
-        max_err = np.amax(Vmag_err)
-        mean_err = np.mean(Vmag_err)
-        payload["ts"]["origin"] = message["ts"]["origin"]
-        payload["sequence"] = message["sequence"]
-        payload["data"] = np.append(values, values)
-        payload["data"] = np.append(payload["data"], [max_err, mean_err])
-        payload["data"] = np.append(payload["data"], [scenario_flag])
-        payload["data"][index] = Vmag_est
-        payload["data"][index + 1] = Vphase_est
-        payload["data"] = list(payload["data"])
-        print(payload)
-        """
-
-        # Finished message
-        print("Finished state estimation for sequence " + str(sequence))
+			#calculate the measured values (affected by uncertainty)
+			measurements_set.meas_creation(dist="uniform", seed=sequence)
+			#Performs state estimation
+			state_estimation_results = DsseCall(system, measurements_set)
 
 
-# grid files
+			#send results to message broker
+			payload = sendVillasNodeOutput(message, output_mapping_vector, powerflow_results, state_estimation_results, scenario_flag)
+			mqttc.publish(topic_publish, "[" + json.dumps(payload) + "]", 0)
+			#print(payload)
+			
+		except Exception as e:
+			print(e)
+		
+		# Finished message
+		print("Finished state estimation for sequence " + str(sequence))
+		
+def receiveVillasNodeInput(system, message, input_mapping_vector):
+	"""
+	function to store the received data in powerflow_results
+	
+	@system: model of the system (type acs.state_estimation.network.System)
+	@param message: received message  from the server (json.loads(msg.payload)[0])
+	@param input_mapping_vector: vector that mapping the result of read_mapping_file
+	@return powerflow_results: object (type acs.state_estimation.results.Results) to store the received data
+	"""
+	data = message['data']
+
+	#create a results object to store the received data
+	powerflow_results = Results(system)
+
+	#store the received data in powerflow_results
+	for node in powerflow_results.nodes:
+		magnitude = 0.0
+		phase = 0.0
+		uuid = node.topology_node.uuid
+		for idx, elem in enumerate(input_mapping_vector):
+			#print("elem[0]: {}, uuid: {}".format(elem[0], uuid))
+			if elem[0] == uuid:
+				if elem[2] == "mag":  # elem[1] = "mag" or "phase"
+					magnitude = data[idx]
+				elif elem[2] == "phase":
+					phase = data[idx]
+		node.voltage = magnitude * (np.cos(phase) + 1j * np.sin(phase)) / 1000
+		node.voltage_pu = node.voltage / node.topology_node.baseVoltage
+	
+	#calculate quantities I, Iinj, S and Sinj
+	powerflow_results.calculate_all()
+	
+	return powerflow_results
+	
+def sendVillasNodeOutput(message, output_mapping_vector, powerflow_results, state_estimation_results, scenario_flag):
+	"""
+	function to create the payload to send 
+	
+	@param message: received message  from the server (json.loads(msg.payload)[0])
+	@param output_mapping_vector: 
+	@param powerflow_results:
+	@param state_estimation_results:
+	@param scenario_flag:
+	@return payload:
+	"""
+	
+	payload = {}
+	payload["ts"] = {}
+	payload["ts"]["origin"] = message["ts"]["origin"]
+	payload["sequence"] = message["sequence"]
+	
+	#calculate Vmag_err
+	Vmag_err = np.zeros(len(powerflow_results.nodes))
+	for idx, elem in enumerate(powerflow_results.nodes):
+		uuid_pf = elem.topology_node.uuid
+		Vmag_true = np.absolute(elem.voltage)
+		Vmag_est = np.absolute(state_estimation_results.get_node(uuid=uuid_pf).voltage)
+		Vmag_err[idx] = 100*np.abs(((Vmag_est - Vmag_true)/Vmag_true))
+		Vmag_err[idx] = 100*((Vmag_est - Vmag_true)/Vmag_true)
+	
+	max_err = np.amax(Vmag_err)
+	mean_err = np.mean(Vmag_err)
+	
+	data = [None]*len(output_mapping_vector)
+	for idx, elem in enumerate(output_mapping_vector):
+		if elem[0] == "max_err":
+			data[idx] = max_err
+			continue
+		elif elem[0] == "mean_err":
+			data[idx] = mean_err
+			continue
+		elif elem[0] == "scenario_flag":
+			data[idx] = scenario_flag
+			continue
+		else:	#elem = ["N4", "V", "phase", "est"] or elem = ["N4", "V", "phase", "pf"]
+			node = None
+			if elem[3] == "est":
+				node = state_estimation_results.get_node(uuid=elem[0])
+			elif elem[3] == "pf":
+				node = powerflow_results.get_node(uuid=elem[0])
+			
+			value = None
+			if elem[2] == "mag":  # elem_data[2] = "mag" or "phase"
+				value = np.absolute(node.voltage)
+			elif elem[2] == "phase":
+				value = np.angle(node.voltage)
+			data[idx] = value
+	
+	payload["data"] = data
+	return payload
+	
+def read_mapping_file(mapping_file):
+	"""
+	To read villas_node_input_data.config or villas_node_output_data.config
+	"""
+	lines = []
+	with open(mapping_file) as mfile:
+		for line in mfile:
+			lines.append(line.strip('\n'))
+	mapping = [None] * len(lines)
+	for pos, elem in enumerate(lines):	
+		mapping[pos] = elem.split(".")
+		
+	return mapping
+	
+#grid files
 xml_files = [
-    r"..\..\state-estimation\examples\quickstart\sample_data\Rootnet_FULL_NE_06J16h_EQ.xml",
-    r"..\..\state-estimation\examples\quickstart\sample_data\Rootnet_FULL_NE_06J16h_SV.xml",
-    r"..\..\state-estimation\examples\quickstart\sample_data\Rootnet_FULL_NE_06J16h_TP.xml"]
+	r"..\..\state-estimation\examples\quickstart\sample_data\Rootnet_FULL_NE_06J16h_EQ.xml",
+	r"..\..\state-estimation\examples\quickstart\sample_data\Rootnet_FULL_NE_06J16h_SV.xml",
+	r"..\..\state-estimation\examples\quickstart\sample_data\Rootnet_FULL_NE_06J16h_TP.xml"]
 
-# measurements files
+#measurements files
 meas_configfile1 = r"..\configs\Measurement_config2.json"
 meas_configfile2 = r"..\configs\Measurement_config3.json"
 
-# load grid
+#read mapping file and create mapping vectors
+input_mapping_file = r"..\configs\villas_node_input_data.conf"
+output_mapping_file = r"..\configs\villas_node_output_data.conf"
+input_mapping_vector = read_mapping_file(input_mapping_file)
+output_mapping_vector = read_mapping_file(output_mapping_file)
+
+#load grid
 Sb = 25
 res = cimpy.cimread(xml_files)
 system = System()
 system.load_cim_data(res, Sb)
 
-# create a results object to store the received data
-powerflow_results = Results(system)
-
-# read mapping file and split each line of mapping_file by point e.g.: N0.V.mag -> ["V0", "mag"]
-mapping_file = r"..\configs\villas_node_input_data.conf"
-mapping = []
-with open(mapping_file) as mfile:
-    for line in mfile:
-        mapping.append(line.strip('\n'))
-for num, elem in enumerate(mapping):
-    uuid, V, type = elem.split(".")
-    mapping[num] = [None] * 2
-    mapping[num][0] = uuid
-    mapping[num][1] = type
-
-##topic names
+client_name = "SognoDemo_Client"
 topic_subscribe = "dpsim-powerflow"
 topic_publish = "sogno-estimator"
 
-'''
 # Public Message Broker
-broker_adress = "m16.cloudmqtt.com"
-mqtt.Client.connected_flag=False						#create flag in class
-mqttc = mqtt.Client("SognoDemo_Client", True)		   	#create new instance
-mqttc.username_pw_set("ilgtdaqk", "UbNQQjmcUdqq")
-mqttc.on_connect = on_connect                   		#attach function to callback
-mqttc.connect(broker_adress, 14543)					 	#connect to broker
-'''
+broker_address = "m16.cloudmqtt.com"
+mqtt_username = "ilgtdaqk"
+mqtt_password = "UbNQQjmcUdqq"
+port = 14543
 
+"""
 # ACS Message Broker
 broker_address = "137.226.248.91"
-mqtt.Client.connected_flag = False                      # create flag in class
-mqttc = mqtt.Client("SognoStateEstimator", True)                  # create new instance
-mqttc.username_pw_set("villas", "s3c0sim4!")
-mqttc.on_connect = on_connect                           # attach function to callback
-mqttc.connect(broker_address)                            # connect to broker
+mqtt_username = "villas"
+mqtt_password = "s3c0sim4!"
+"""
 
-mqttc.on_message = on_message                           # attach function to callback
-mqttc.loop_start()                                      # start loop to process callback
-time.sleep(4)                                           # wait for connection setup to complete
-mqttc.subscribe(topic_subscribe)
+mqttc = connect(client_name, mqtt_username, mqtt_password, broker_address, port)
 
-input("Press enter to stop client...")
-
-mqttc.loop_stop()  # Stop loop
-mqttc.disconnect()  # disconnect
+input("Press enter to stop client...\n")
+mqttc.loop_stop()  								# Stop loop
+mqttc.disconnect()  							# disconnect
