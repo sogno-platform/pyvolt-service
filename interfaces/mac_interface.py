803f3f95010481573469b92ec19b31c7d5fec04f
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2020-03-02 14:59:55 +0000

    specify mac message conversion for real and imag power

--- interfaces/mac_interface.py
+++ interfaces/mac_interface.py
@@ -11,37 +11,33 @@ from json import dumps, loads
 import numpy as np
 
 
-def convertMacMessageToSognoMessage(message):
+def convertMacMessageToSognoMessage(mac_message, device_type_to_comp):
     """
     TBD
     """
 
-    mac_message = loads(message.payload)
-
+    # Mapping of terminology: from mac source to sogno phase
     source_to_phase = {"channel_1": "A", "channel_2": "B", "channel_3": "C", "channel_N": "N"}
-    device_type_to_comp = {  ("862877034274926-1", "volt"): "_TopologicalNode_STN-032187Y",
-                             ("865374038674019-1", "volt"): "_TopologicalNode_85946"}
 
     sogno_message = {}
     sogno_message["device"] = mac_message["identifier"]
     sogno_message["timestamp"] = mac_message["timestamp"]
     sogno_message["readings"] = []
-
     mac_readings = mac_message["readings"]
-
     for mac_elem in mac_readings:
         sogno_elem = {}      
         try:
             sogno_elem["component"] = device_type_to_comp[(mac_message["identifier"], mac_elem["type"])]
         except KeyError:
             sogno_elem["component"] = "unspecified"
-            #print("Warning: mapping from ({}, {}, {}) to component not specified. Setting \"component\" field to \"{}\")".format(mac_message["identifier"], mac_elem["source"], mac_elem["type"], sogno_elem["component"]))
-                
+            print("Warning: mapping from ({}, {}) to CIM component not specified.".format(mac_message["identifier"], mac_elem["type"]))
         sogno_elem["measurand"] = mac_elem["type"]
         sogno_elem["phase"] = source_to_phase[mac_elem["source"]]
-        if sogno_elem["measurand"] == "volt":
-            sogno_elem["data"] = float(mac_elem["data"])
+        if sogno_elem["measurand"] == "activepower" or sogno_elem["measurand"] == "reactivepower":
+            # convert single-phase power in kW to single-phase power in W as expected by SOGNO interface
+            sogno_elem["data"] = float(mac_elem["data"])*1e3
         else:
+            # take data as they are
             sogno_elem["data"] = float(mac_elem["data"])        
         sogno_message["readings"].append(sogno_elem)
 
