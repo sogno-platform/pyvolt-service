997c29e9759fcb9ea319da5c925946f317364875
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2020-03-03 16:15:20 +0000

    Add measurand term mapping to mac interface

--- interfaces/mac_interface.py
+++ interfaces/mac_interface.py
@@ -15,9 +15,19 @@ def convertMacMessageToSognoMessage(mac_message, device_type_to_comp):
     """
     TBD
     """
+    
+
+    print("MAC interface:")
+    for reading in mac_message['readings']:
+        print("Received measurement value: {}, {}, {}, {}, {}".format(mac_message['timestamp'], mac_message['identifier'], reading['type'], reading['source'], reading['data']))
 
     # Mapping of terminology: from mac source to sogno phase
-    source_to_phase = {"channel_1": "A", "channel_2": "B", "channel_3": "C", "channel_N": "N"}
+    source_to_phase = {"channel_1": "A", "channel_2": "B", "channel_3": "C"}
+
+    # Mapping of terminology: from mac type to sogno measurand
+    type_to_measurand = {"volt": "voltmagnitude",
+                         "activepower": "activepower",
+                         "reactivepower": "reactivepower"}
 
     sogno_message = {}
     sogno_message["device"] = mac_message["identifier"]
@@ -26,19 +36,33 @@ def convertMacMessageToSognoMessage(mac_message, device_type_to_comp):
     mac_readings = mac_message["readings"]
     for mac_elem in mac_readings:
         sogno_elem = {}      
-        try:
+
+        # Supplement mac reading by CIM component UUID related to device and measurand
+        if (mac_message["identifier"], mac_elem["type"]) in device_type_to_comp.keys():
             sogno_elem["component"] = device_type_to_comp[(mac_message["identifier"], mac_elem["type"])]
-        except KeyError:
+        else:
             sogno_elem["component"] = "unspecified"
             print("Warning: mapping from ({}, {}) to CIM component not specified.".format(mac_message["identifier"], mac_elem["type"]))
-        sogno_elem["measurand"] = mac_elem["type"]
-        sogno_elem["phase"] = source_to_phase[mac_elem["source"]]
+
+        # Map terms for type and source, if mapping available otherwise skip reading
+        if mac_elem["type"] in type_to_measurand.keys():
+            sogno_elem["measurand"] = type_to_measurand[mac_elem["type"]]
+        else:
+            pass
+        if mac_elem["source"] in source_to_phase.keys():
+            sogno_elem["phase"] = source_to_phase[mac_elem["source"]]
+        else:
+            pass
+
+        # Actual measurement data assignment including necessary value conversion from MAC to SOGNO format
         if sogno_elem["measurand"] == "activepower" or sogno_elem["measurand"] == "reactivepower":
             # convert single-phase power in kW to single-phase power in W as expected by SOGNO interface
             sogno_elem["data"] = float(mac_elem["data"])*1e3
         else:
             # take data as they are
             sogno_elem["data"] = float(mac_elem["data"])        
+        
+        # Add element to output message in SOGNO format
         sogno_message["readings"].append(sogno_elem)
 
     return sogno_message
